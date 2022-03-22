3b9078548f9951d9c295213c6a106c2ac5d91cb1
Douglas Barcellos <84218639+Dougxiii@users.noreply.github.com>
2022-03-22 17:10:44 -0300

    final

--- Teste1/src/Teste1.java
+++ Teste1/src/Teste1.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Teste1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int first = leitor.nextInt();
+        int last = leitor.nextInt();
+
+        for(int i = first; i <= last; i++){
+            if(i % 2!=0){
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
---
+++ Teste3/src/Conta.java
@@ -0,0 +1,8 @@
+public class Conta {
+
+    //atributos
+
+    private int numero;
+    private String titular;
+    private double saldo;
+}
